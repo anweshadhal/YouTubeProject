12a7ffc08786ed2f32d820b66bf5558bc8bfbf5f
anweshadhal <anwesha.dhal95@gmail.com>
2024-09-21 11:37:47 +0530

    Runner file name change

--- YouTubeProject/src/test/java/runners/TestYouTubeRunner.java
+++ YouTubeProject/src/test/java/runners/TestYouTubeRunner.java
@@ -5,16 +5,17 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features="src/test/resources/Features/YouTubeSearch.feature",
 		glue= {"StepDefinitions"},
 		monochrome=true,
-		plugin= {"pretty", "html:target/cucumber", "json:target/cucumber.json"},
+		plugin = {"json:target/cucumber.json"},
 		tags="@YouTubeMusicSearch"
 		
 		)
-public class YouTubeRunner {
+public class TestYouTubeRunner {
 
 }
 
